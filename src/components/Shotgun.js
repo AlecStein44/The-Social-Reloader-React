12f0176949d9db1fd62d5617fdc86362505cc2a0
AlecStein44 <45604055+AlecStein44@users.noreply.github.com>
2019-06-07 00:27:15 -0700

    Update Shotgun.js

--- src/components/Shotgun.js
+++ src/components/Shotgun.js
@@ -12,7 +12,7 @@ class Shotgun extends React.Component {
       }
 
     componentDidMount() {
-        fetch(`http://localhost:8000/Shotgun`)
+        fetch(`https://the-social-reloader-server.herokuapp.com/shotgun`)
       .then(response => response.json())
       .then(data => 
         this.setState({
