b2e3bb76e7c2c3b9297ddd61993f6bcc802421f6
AlecStein44 <45604055+AlecStein44@users.noreply.github.com>
2019-06-07 00:26:04 -0700

    Update ContPistolRifle.js

--- src/components/ContPistolRifle.js
+++ src/components/ContPistolRifle.js
@@ -6,7 +6,7 @@ const ContPistolRifle = (props) => {
         <div className="rps-div">
             <Link to={{pathname: "/data", search: `?find=${props.page}`}} className="rps-h2">Name: {props.name}</Link>
             <div className="rps-main">
-                <img src={`../uploads/${props.img}`} className="rps-img" width="300px" height="250px" />
+                <img src={`./uploads/${props.img}`} className="rps-img" width="300px" height="250px" />
                 <div className="rps-info">
                 <p>Caliber: {props.cal}</p>
                 <p>Bullet: {props.bullet}</p>
