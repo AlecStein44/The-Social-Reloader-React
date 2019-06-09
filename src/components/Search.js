04e4da11568b7a00c4b45fb9a48efb31343f59d6
AlecStein44 <45604055+AlecStein44@users.noreply.github.com>
2019-06-09 14:17:48 -0700

    Update Search.js

--- src/components/Search.js
+++ src/components/Search.js
@@ -52,10 +52,10 @@ class Search extends React.Component {
                         <section className="rps-sec">
                             <h2>Search Results</h2>
                             {(this.state.resultsShotgun.length !== 0) ? this.state.resultsShotgun.map(data => {
-                                return <ContShotgun page={data.id} img={data.img} name={data.name} user={data.user} gauge={data.gauge} shot={data.shot} primer={data.primer} shell={data.shell} price={data.price} fps={data.fps} min={data.min} avg={data.avg} max={data.max}  />
+                                return <ContShotgun page={data.id} img={data.img} name={data.name} user={data.user} gauge={data.gauge} shot={data.shot} powder={data.powder} primer={data.primer} shell={data.shell} price={data.price} fps={data.fps} min={data.min} avg={data.avg} max={data.max}  />
                             }) : console.log('No Shotgun Found')}
                             {this.state.isThereData ? this.state.resultsRifle.map(data => {
-                            return <ContPistolRifle page={data.id} img={data.img} name={data.name} user={data.user} cal={data.caliber} bullet={data.bullet} primer={data.primer} brandcase={data.brandcase} price={data.price} fps={data.fps} min={data.min} avg={data.avg} max={data.max}  /> })
+                            return <ContPistolRifle page={data.id} img={data.img} name={data.name} user={data.user} cal={data.caliber} bullet={data.bullet} powder={data.powder} primer={data.primer} brandcase={data.brandcase} price={data.price} fps={data.fps} min={data.min} avg={data.avg} max={data.max}  /> })
                                 : <h3>Sorry We Couldn't Find your Reloads</h3>}
                         </section>
                     </main>
